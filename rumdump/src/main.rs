0dd9b5761ef0c1ede5b5def7db1eca49ab84b001
Michaela Healy <michaela_healy@uri.edu>
2022-11-29 22:35:13 -0500

    Main complete

--- rumdump/src/main.rs
+++ rumdump/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
-}
+use std::env;
+
+use rumdump::rumdis;
+use rumdump::rumload;
+
+    fn main() {
+        let input = env::args().nth(1);
+        let instructions = rumload::load(input.as_deref());
+        println!("{} instructions", instructions.len());
+        for instruction in instructions {
+            println!("{}", rumdis::disassemble(instruction))
+        }
+    }
